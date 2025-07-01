7c16a776acb2a7fbcbf189b195fa176e499f033b
KEVIN-NGUYEN <nguyenkhoatdtu303@gmail.com>
2025-07-01 11:10:13 +0700

    Add function to get random color
    Create a function to getColor() using Math.random to get random color code.

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 let boxes =  document.querySelectorAll('.box');
+const hexaCode = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+
 
 boxes.forEach(box => {
     box.addEventListener('mouseenter', () => {
-        box.style.backgroundColor = 'black';
+        box.style.backgroundColor = getColor();
     })
 })
+
+
+function getRandomIndex() {
+    return Math.floor(Math.random() * hexaCode.length);
+}
+
+function getColor() {
+    let colorCode = '#';
+    
+    for (let i = 0; i < 6; i++) {
+        let index = getRandomIndex();
+        colorCode += hexaCode[index];
+    }
+
+    return colorCode;
+}
